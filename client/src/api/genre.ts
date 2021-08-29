34cadf6a32696628362a6c858ca6722709cae716
khudiiash <dmytro.khudiiash@gmail.com>
2021-08-29 10:08:09 +0300

    adjustments

--- client/src/api/genre.ts
+++ client/src/api/genre.ts
@@ -1,12 +1,16 @@
 import store from '@/store'
 import axios from 'axios'
+import gsap from 'gsap/all'
 
 export const genre = (url: string) => {
         store.hideBookScreen()
+        store.state.isLoading = true
+        gsap.set('.search-column', {opacity: 0})
         axios.post(`/genre`, {url})
         .then(res => {
             store.setSearchResult(res.data.books)
             store.showSearchScreen()
+            store.state.isLoading = false
         })
         .catch(err => console.log(err))
     } 
